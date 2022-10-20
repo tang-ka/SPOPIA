c966002897d9a1897a07bee33b48a25acd4ea065
tang-ka <gur0907@hanyang.ac.kr>
2022-10-21 03:20:40 +0900

    ..

--- Assets/SH/Script/DataManager.cs
+++ Assets/SH/Script/DataManager.cs
@@ -2,17 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DataManager : MonoBehaviour
+namespace SpopiaData
 {
-    // Start is called before the first frame update
-    void Start()
+    public class UserData
     {
-        
+        // user info
+        string nickName;
+        int age;
+        int playerLevel; // 0: normal, 2 : middle, 3: high, 4: university 5: K2, 6: K1
+        string position;
+        string teamName;
+        int height;
+        int weight;
+        string profileImage;
+
+        //int 
+        //int backNumber;
+        //string favoritePlayer;
+        //Avatar
+
+        // user record
+        int goal;
+        int assist;
+        int matchCount;
+
+        //int SPOPIAscore : 개인 전투력
+        //recent5
     }
 
-    // Update is called once per frame
-    void Update()
+    public class TeamData
     {
-        
+        // team info
+        int memberNum;
+        string region;
+        int[] levelerCount = new int[5];
+        string formation; // 4-4-2, 4-2-3-1
+
+        // team record
+        int goal;
+        int lossGoal;
+        int matchCount;
+        int win;
+        int lose;
+        int draw;
+
+        //int SPOPIAscore : 팀 전투력
+        //recent5
     }
 }
