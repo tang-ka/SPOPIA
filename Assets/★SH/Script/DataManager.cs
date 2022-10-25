06a08e8f6e58583594441d49aaf5de87dc6f69c9
tang-ka <gur0907@hanyang.ac.kr>
2022-10-25 19:15:57 +0900

    ..

--- Assets/★SH/Script/DataManager.cs
+++ Assets/★SH/Script/DataManager.cs
@@ -2,51 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace SpopiaData
+public class UserData
 {
-    public class UserData
-    {
-        // user info
-        string nickName;
-        int age;
-        int playerLevel; // 0: normal, 2 : middle, 3: high, 4: university 5: K2, 6: K1
-        string position;
-        string teamName;
-        int height;
-        int weight;
-        string profileImage;
+    // user info
+    string nickName;
+    int age;
+    int playerLevel; // 0: normal, 2 : middle, 3: high, 4: university 5: K2, 6: K1
+    string position;
+    string teamName;
+    int height;
+    int weight;
+    string profileImage;
 
-        //int 
-        //int backNumber;
-        //string favoritePlayer;
-        //Avatar
+    //int 
+    //int backNumber;
+    //string favoritePlayer;
+    //Avatar
 
-        // user record
-        int goal;
-        int assist;
-        int matchCount;
+    // user record
+    int goal;
+    int assist;
+    int matchCount;
 
-        //int SPOPIAscore : 개인 전투력
-        //recent5
-    }
+    //int SPOPIAscore : 개인 전투력
+    //recent5
+}
 
-    public class TeamData
-    {
-        // team info
-        int memberNum;
-        string region;
-        int[] levelerCount = new int[5];
-        string formation; // 4-4-2, 4-2-3-1
+public class TeamData
+{
+    // team info
+    int memberNum;
+    string region;
+    int[] levelerCount = new int[5];
+    string formation; // 4-4-2, 4-2-3-1
 
-        // team record
-        int goal;
-        int lossGoal;
-        int matchCount;
-        int win;
-        int lose;
-        int draw;
+    // team record
+    int goal;
+    int lossGoal;
+    int matchCount;
+    int win;
+    int lose;
+    int draw;
 
-        //int SPOPIAscore : 팀 전투력
-        //recent5
-    }
+    //int SPOPIAscore : 팀 전투력
+    //recent5
 }
---
+++ Assets/★SH/Script/MyLeagueData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyLeagueData : MonoBehaviour
+{
+    //public TeamData[] 
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
