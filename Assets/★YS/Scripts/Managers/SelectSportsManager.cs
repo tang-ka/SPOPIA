d36c39e7d138719cb7fec8e5da7cd238f11665cf
SeoYeong-su <mvp0340@gmail.com>
2022-11-04 13:53:45 +0900

    221104 작업 - 씬 연결 작업 중

---
+++ Assets/★YS/Scripts/Managers/SelectSportsManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectSportsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            SelectSports();
+        }
+    }
+
+    void SelectSports()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            if(hit.transform.gameObject.name == "Soccer")
+            {
+                SceneManager.LoadScene("YS_MyLeagueScene");
+            }
+        }
+    }
+}
