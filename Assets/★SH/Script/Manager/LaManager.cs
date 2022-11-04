60fa967b55527b0e09b4346099f0f3f3145f16e8
tang-ka <gur0907@hanyang.ac.kr>
2022-11-04 16:44:18 +0900

    ..

--- Assets/★SH/Script/Manager/LaManager.cs
+++ Assets/★SH/Script/Manager/LaManager.cs
@@ -1,18 +1,50 @@
+using Photon.Pun;
+using PlayFab.MultiplayerModels;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LaManager : MonoBehaviour
+public class LaManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
+    public static LaManager instance;
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    // Spawn Position
+    public Vector3 spawnPos;
+
     void Start()
+    {
+        // 게임씬에서 다음씬으로 넘어갈때 동기화해주기 ( 게임씬 등에서 한번 )
+        PhotonNetwork.AutomaticallySyncScene = true;
+
+        // OnPhotonSerializeView 호출 빈도
+        PhotonNetwork.SerializationRate = 60;
+        // RPC 호출 빈도
+        PhotonNetwork.SendRate = 60;
+
+        SettingSpawnOption();
+
+        CreateUser();
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SettingSpawnOption()
     {
-        
+        spawnPos = Vector3.zero + Vector3.up * 3;
+    }
+
+    public void CreateUser()
+    {
+
+        // 플레이어를 생성한다.
+        PhotonNetwork.Instantiate("Player", spawnPos, Quaternion.identity);
     }
 }
---
+++ Assets/★SH/Script/SH_CheatKey.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SH_CheatKey : MonoBehaviour
+{
+    public InputField id;
+    public InputField pw;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            id.text = "gur0907@hanyang.ac.kr";
+            pw.text = "gur135";
+        }
+    }
+}
